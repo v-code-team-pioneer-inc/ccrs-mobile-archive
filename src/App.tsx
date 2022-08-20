e727635a41cd4f208d595ae7eb20ecd2315a4d6a
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 03:56:40 +0900

    [ADD]タイトル

--- src/App.tsx
+++ src/App.tsx
@@ -70,25 +70,11 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={styles.header}>中央大学教室予約システム</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -111,6 +97,12 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
 
